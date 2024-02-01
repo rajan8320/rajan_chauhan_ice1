cdb8f5f35d8f87d71b88a2c21c06d3ee67860e4d
rajan8320 <chrajanj@sheridancollege.ca>
2024-02-01 15:04:34 -0800

    ice1

---
+++ src/rajan_chauhan_1/Card.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rajan_chauhan_1;
+
+/**
+ *
+ * @author Rajan chauhan
+ * Name=rajan jitendrasinh chauhan
+ * student id = 991704597
+ */
+public class Card {
+
+   private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+   
+   
+    
+}
